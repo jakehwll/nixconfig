fffd3e442b982aa58a2b2b1037d6890e4607176c
Jake Howell <jake@hwll.me>
2022-12-06 08:37:14 +1100

    fix: enable experimental features (flakes)

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -120,6 +120,9 @@
     teams
   ];
 
+  # Experimentals
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
