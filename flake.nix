40349fd5f4fa673810ca950ae9a04eb3e4a98e6e
Jake Howell <jake@hwll.me>
2023-04-08 10:52:04 +1000

    fix: remove hyprland from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
